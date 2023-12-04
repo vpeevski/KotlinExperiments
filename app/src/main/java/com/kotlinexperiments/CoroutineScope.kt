9bcbf50a7d606ef50bffea83c226d28677144735
Vasil Peevski <v.peevski@allterco.com>
2023-12-04 12:31:58 +0200

    Initial commit.

---
+++ app/src/main/java/com/kotlinexperiments/CoroutineScope.kt
@@ -0,0 +1,12 @@
+package com.kotlinexperiments
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+//suspend fun main(): Unit = coroutineScope {
+//    launch {
+//        delay(1000L)
+//        println("World!")
+//    }
+//    println("Hello,")
+//}
---
+++ app/src/main/java/com/kotlinexperiments/Coroutines.kt
@@ -0,0 +1,81 @@
+package com.kotlinexperiments
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlin.random.Random
+
+//suspend fun main() {
+//    println("Before")
+////    suspendCoroutine<Unit> { continuation ->
+////        thread {
+////            println("Suspended")
+////            Thread.sleep(2000)
+////            continuation.resume(Unit)
+////            println("Resumed")
+////        }
+//
+////    }
+////    delay(1000)
+//    val user1 = requestUserRepo()
+//    println("### Local User: $user1")
+//    val result = runCatching { requestUserRepo(success = false) }
+//    println("### Result: $result")
+//    println("After")
+//}
+
+val executor = Executors.newSingleThreadScheduledExecutor {
+    Thread(it, "scheduler").apply {
+        isDaemon = true
+    }
+}
+
+suspend fun delay(timeMillis: Long) = suspendCoroutine { continuation ->
+//    println("Suspended")
+    executor.schedule({
+        continuation.resume(Unit)
+    }, timeMillis, TimeUnit.MILLISECONDS)
+//    println("Resumed")
+}
+
+suspend fun delayCancelable(timeMillis: Long) = suspendCancellableCoroutine { continuation ->
+    println("Suspended")
+    executor.schedule({
+        continuation.resume(Unit)
+    }, timeMillis, TimeUnit.MILLISECONDS)
+    println("Resumed")
+}
+
+suspend fun requestUserRepo(success: Boolean = true): ApiResponse<User> {
+    return suspendCancellableCoroutine { cont ->
+        requestUserWithCallback(responseCallback = { apiResponse ->
+            println(apiResponse)
+            when (apiResponse) {
+                is ApiResponse.Success -> cont.resume(apiResponse)
+                is ApiResponse.Error -> cont.resumeWithException(apiResponse.exception)
+            }
+        }, succeed = success)
+    };
+}
+
+fun requestUserBlocking(): User {
+    Thread.sleep(2000)
+    return User(id = Random(1000).nextInt(), name = "Ivan")
+}
+
+fun requestUserWithCallback(responseCallback: (ApiResponse<User>) -> Unit, succeed: Boolean) {
+    Thread.sleep(2000)
+    return if (succeed)
+        responseCallback(ApiResponse.Success(User(id = Random(1000).nextInt(), name = "Ivan")))
+    else responseCallback(ApiResponse.Error(java.lang.Exception("Api Error")))
+}
+
+data class User(val id: Number, val name: String)
+
+sealed class ApiResponse<out T> {
+    data class Success<R>(val data: R) : ApiResponse<R>()
+    data class Error(val exception: Exception) : ApiResponse<Nothing>()
+}
